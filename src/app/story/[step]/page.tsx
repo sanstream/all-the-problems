c16708ce4745b20afc8a19e3d4eae0fa382ec960
Sanne <sanne@bankai.eu>
2023-08-19 11:22:18 +0200

    Second page setup

---
+++ src/app/story/[step]/page.tsx
@@ -0,0 +1,15 @@
+export default function DefaultStory() {
+  return (
+    <>
+      <header>
+        <h1>All the problems in the worlds</h1>
+      </header>
+      <figure>
+        <svg></svg>
+        <figcaption>
+        <p>You can explore the visualization further by clicking on any of the major problems.</p>
+        </figcaption>
+      </figure>
+    </>
+  )
+}
